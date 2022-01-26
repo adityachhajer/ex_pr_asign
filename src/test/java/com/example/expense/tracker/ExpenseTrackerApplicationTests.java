fb79de416d5c6f9359a8d97b330a41657604f9f2
Aditya Chhajer <chhajeraditya@gmail.com>
2022-01-27 01:07:50 +0530

    Modified test case to run with env variables.

--- src/test/java/com/example/expense/tracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/com/example/expense/tracker/ExpenseTrackerApplicationTests.java
@@ -2,8 +2,15 @@ package com.example.expense.tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = ExpenseTrackerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"port=3306",
+		"db_name=expense_tracker",
+		"username=root",
+		"password=root"})
+@ActiveProfiles("test")
 class ExpenseTrackerApplicationTests {
 
 	@Test
